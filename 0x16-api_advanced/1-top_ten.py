5849ed453360416beab8d618e1907e35c0dbb15a
middo1 <hamidadeshina1@gmail.com>
2022-11-02 14:03:41 +0100

    pycodestyle compliant

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,12 +9,14 @@ def top_ten(subreddit):
     headers = {
         "User-Agent": "linux:0x16.ap1.advanced.v1.0.0 (by middo)"
     }
-    params  = {
-        "limit":10
+    params = {
+        "limit": 10
     }
-    res = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    res = requests.get(url, headers=headers,
+                       params=params, allow_redirects=False)
     if res.status_code == 404:
         print('None')
         return
     results = res.json().get("data")
-    [print(child.get("data").get("title")) for child in results.get("children")]
+    [print(child.get("data").get("title"))
+     for child in results.get("children")]
