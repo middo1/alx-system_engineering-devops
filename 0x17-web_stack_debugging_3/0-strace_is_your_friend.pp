a2c90ffd3aef453a53cd90c647d526daa2a52e87
middo1 <hamidadeshina1@gmail.com>
2022-11-07 16:37:52 +0100

    debugging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fix error 500 when a GET HTTP method is requestednto Apache server
+
+exec { 'replace':
+    provider => shell,
+    command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+}
